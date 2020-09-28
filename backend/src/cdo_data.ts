049918df7c03adf60bae2b9246ced901b95bd2f4
Josh Cardenzana <jcardenzana@peregrineadvisors.com>
2020-09-28 14:33:15 -0400

    Add the files defining the cdo_data loaders and how to compile them These are compilable to JavaScript for use in the portal

---
+++ backend/src/cdo_data.ts
@@ -0,0 +1,43 @@
+/****************************************************************************
+ * @file cdo_data.ts
+ * 
+ * This file is responsible for detailing how to load and parse the data
+ ****************************************************************************
+ * Copyright (c): 2020
+ ****************************************************************************/
+
+/**
+ * Interface defining data associated with a CDO
+ */
+interface cdo {
+    agency: {
+        name: string;
+        abbrv: string;
+        image: string;
+        data: string;
+        website: string;
+    },
+    name: {
+        first: string;
+        last: string;
+    },
+    contact: {
+        website: string;
+        email: string;
+        linkedin: string;
+    }
+};
+
+
+/**
+ * Helper function for loading CDO data from properly formatted JSON file
+ * @param database_url URL where JSON database file can be loaded
+ */
+async function getCdoDatabase(database_url: string): Promise<cdo[]> {
+    // Load the database file
+    let data_json = await fetch(database_url)
+                            .then(response => {return response.json()});
+    let cdoDatabase:cdo[] = data_json.data;
+
+    return cdoDatabase;
+};
